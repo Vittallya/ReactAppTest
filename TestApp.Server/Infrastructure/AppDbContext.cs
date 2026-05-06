132fe584056aeb7d6e582edd2fe6734d7a019951
Vittallya <vit.por@bk.ru>
2026-05-06 20:09:32 +0300

    seed data

--- TestApp.Server/Infrastructure/AppDbContext.cs
+++ TestApp.Server/Infrastructure/AppDbContext.cs
@@ -24,5 +24,19 @@ public class OrderSchema : IEntityTypeConfiguration<Order>
         builder.Property(x => x.SenderAddress).HasMaxLength(150);
         builder.Property(x => x.RecieverCity).HasMaxLength(50);
         builder.Property(x => x.RecieverAddress).HasMaxLength(150);
+
+        builder.HasData(new[]
+        {
+            new Order(1, "Moscow", "Kremlin", "SPb", "petrogradskaya naberezhnaya", 8
+                , new DateTimeOffset(new DateTime(2009, 1, 1, 12, 0, 0, 0, DateTimeKind.Utc))),
+            new Order(2, "Kazan", "Kremlin", "SPb", "Vasilievskiy Island", 2
+                , new DateTimeOffset(new DateTime(2020, 1, 1, 12, 0, 0, 0, DateTimeKind.Utc))),
+            new Order(3, "Krasnodar", "Vokzal", "Moscow", "City Moll", 15
+                , new DateTimeOffset(new DateTime(2019, 1, 1, 12, 0, 0, 0, DateTimeKind.Utc))),
+            new Order(4, "Astana", "Vokzal", "Moscow", "City Moll", 12
+                , new DateTimeOffset(new DateTime(2020, 1, 1, 12, 0, 0, 0, DateTimeKind.Utc))),
+            new Order(5, "Orel", "Vokzal", "Moscow", "City Moll", 12
+                , new DateTimeOffset(new DateTime(2021, 1, 1, 12, 0, 0, 0, DateTimeKind.Utc))),
+        });
     }
 }
